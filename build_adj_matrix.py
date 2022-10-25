3358f5bd962b50ee70b07235a1d4a67e9aebadb2
Ataberk Donmez <ataberdonmez@gmail.com>
2022-10-25 00:12:44 -0400

    apply suggested style changes, remove redundant distance_clusters function

--- build_adj_matrix.py
+++ build_adj_matrix.py
@@ -10,7 +10,6 @@ def build_adj_matrix(cl, data, SNP_pos, I, file, edge, R, only_with_common_snip=
             for j in range(1, m.shape[1]):
                 second_read=m.index[j]
                 m[second_read][first_read] = distance(first_read, second_read, data, SNP_pos, R, only_with_common_snip=False)
-
     else:
         for i in range(1, m.shape[1]):
             print(str(i) + "/" + str(m.shape[1]) + " Reads processed \r", end="")
@@ -25,17 +24,18 @@ def build_adj_matrix(cl, data, SNP_pos, I, file, edge, R, only_with_common_snip=
 
             for pos in [border1, border2]:
                 for pileupcolumn in bamfile.pileup(edge, int(pos) - 1, int(pos), stepper='samtools',
-                                               ignore_overlaps=False,
-                                               ignore_orphans=False, truncate=True):
+                                                   ignore_overlaps=False,
+                                                   ignore_orphans=False,
+                                                   truncate=True):
                     for pileupread in pileupcolumn.pileups:
                         second_read = pileupread.alignment.query_name
 
                         try:
                             if m[second_read][first_read] == -1:
                                 m[second_read][first_read] = distance(first_read, second_read, data, SNP_pos, R)
-                        except:
-                            KeyError
-    return (m)
+                        except KeyError:
+                            pass
+    return m
 
 
 def distance(read1, read2, data, SNP_pos, R, only_with_common_snip=True):
@@ -73,13 +73,13 @@ def distance(read1, read2, data, SNP_pos, R, only_with_common_snip=True):
             d = 0
         else:
             d = 1
-    return (d)
+    return d
 
 
 def remove_edges(m, R):
     m_transformed = m
     m_transformed[m_transformed >= R] = -1
-    return (m_transformed)
+    return m_transformed
 
 
 def change_w(m, R):
@@ -87,43 +87,4 @@ def change_w(m, R):
     m_transformed[m_transformed == 0] = 0.001
     m_transformed[m_transformed == -1] = 0
     m_transformed[m_transformed >= R] = 0
-    return (m_transformed)
-
-
-
-def distance_clusters(first_cl,second_cl, cons,SNP_pos, only_with_common_snip=True):
-    d=-1
-    firstSNPs=list(cons[first_cl].keys())
-    secondSNPs = list(cons[second_cl].keys())
-    keys=('clSNP', 'Strange','Stop','Start','Cov')
-    firstSNPs = [key for key in firstSNPs if key not in keys]
-    secondSNPs= [key for key in secondSNPs if key not in keys]
-    commonSNP=sorted(set(firstSNPs).intersection(secondSNPs))
-    try:
-        intersect=set(range(cons[first_cl]["Start"],cons[first_cl]["Stop"])).intersection(set(range(cons[second_cl]["Start"],cons[second_cl]["Stop"])))
-        if only_with_common_snip==False and len(commonSNP)==0 and len(intersect)>0:
-            d=0
-        else:
-            for snp in commonSNP:
-                try:
-                    b1=cons[first_cl][snp]
-                    b2=cons[second_cl][snp]
-                    if b1 != b2 and len(b1)!=0 and  len(b2)!=0:
-                        if d==-1:
-                            d=0
-                        d=d+1
-                    elif b1 == b2:
-                        if d==-1:
-                            d=0
-                        d=d
-                except:
-                    continue
-                if d>=1:
-                    d=1
-                    break
-
-                else:
-                    continue
-    except(IndexError):pass
-    return (d)
-
+    return m_transformed
