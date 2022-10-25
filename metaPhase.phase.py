7101e6a0ccbd8731e93934fccd0cc0f8ade5f57a
Ataberk Donmez <ataberdonmez@gmail.com>
2022-10-25 00:20:12 -0400

    Modified to work with object-oriented flye consensus computation.

--- metaPhase.phase.py
+++ metaPhase.phase.py
@@ -1,40 +1,72 @@
 import multiprocessing
-from params import *
-from cluster import cluster
-from color_bam import color
 import pysam
+import pickle
 import os
 import subprocess
+from multiprocessing.managers import BaseManager
+
+from cluster import cluster
+from color_bam import color
+from flye_consensus import FlyeConsensus
+from params import *
 
 
-dirs = ("output/","output/vcf/","output/adj_M/","output/clusters/","output/graphs/","output/bam/")
+dirs = ("output/",
+        "output/vcf/",
+        "output/adj_M/",
+        "output/clusters/",
+        "output/graphs/",
+        "output/bam/",
+        "output/consensus")
 for dir in dirs:
     try:
         os.stat(dir)
     except:
-         os.mkdir(dir)
+        os.mkdir(dir)
+
+
+class CustomManager(BaseManager):
+    pass
+
 
 def phase(edges):
-    pool = multiprocessing.Pool(3)
-    pool.map(cluster, range(0, len(edges)))
-    pool.close()
+    CustomManager.register('FlyeConsensus', FlyeConsensus)
+
+    with CustomManager() as manager:
+        default_manager = multiprocessing.Manager()
+        lock = default_manager.Lock()
+        empty_consensus_dict = {}
+        shared_flye_consensus = manager.FlyeConsensus(bam, gfa, empty_consensus_dict, lock)
+        pool = multiprocessing.Pool(4)
+        init_args = [(i, shared_flye_consensus) for i in range(len(edges))]
+        pool.map(cluster, init_args)
+        pool.close()
+        shared_flye_consensus.print_cache_statistics()
+        return shared_flye_consensus.get_consensus_dict()
+
+
+def sequential_phase(edges):
+    empty_consensus_dict = {}
+    flye_consensus = FlyeConsensus(bam, gfa, empty_consensus_dict, None)
+    for i in range(len(edges)):
+        cluster((i, flye_consensus))
+    flye_consensus.print_cache_statistics()
+    return flye_consensus.get_consensus_dict()
+
 
 def col(edges):
     pool = multiprocessing.Pool(1)
     pool.map(color, range(0, len(edges)))
     pool.close()
-    subprocess.check_output('samtools merge output/bam/coloredBAM.bam -f `find output/bam -name "*edge*.bam"`', shell=True, capture_output=False)
-    subprocess.check_output('rm `find output/bam -name "*edge*.bam"`', shell=True,capture_output=False)
-    pysam.samtools.index("output/bam/coloredBAM.bam", "output/bam/coloredBAM.bai")
-
-
+    subprocess.check_output('samtools merge output/bam/coloredBAM.bam -f `find output/bam -name "*edge*.bam"`',
+                            shell=True, capture_output=False)
+    subprocess.check_output('rm `find output/bam -name "*edge*.bam"`', shell=True, capture_output=False)
+    pysam.index("output/bam/coloredBAM.bam")
 
 
 if __name__ == "__main__":
-    phase(edges)
+    consensus_dict = phase(edges)
+    if write_consensus_cache:
+        with open(consensus_cache_path, 'wb') as f:
+            pickle.dump(consensus_dict, f)
     col(edges)
-
-
-
-
-
