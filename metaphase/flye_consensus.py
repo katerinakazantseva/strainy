0f477d6ef5ae1cc7faa7829590659629102a047b
Ataberk Donmez <ataberdonmez@gmail.com>
2023-01-05 17:02:01 +0300

    Use generalized formatting for macos-linux inconsistency in the alignment string

--- metaphase/flye_consensus.py
+++ metaphase/flye_consensus.py
@@ -14,6 +14,7 @@ from Bio.SeqRecord import SeqRecord
 from metaphase.params import *
 
 logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
 
 def calculate_coverage(position, read_limits):
     """
@@ -353,7 +354,7 @@ class FlyeConsensus:
         aligner.gap_score = -1
         alignments = aligner.align(first_consensus_clipped, second_consensus_clipped)
         # get the alignment string consisting of (- . |)
-        alignment_string = str(alignments[0]).format().split('\n')[1]
+        alignment_string = str(alignments[0]._format_generalized()).replace(' ', '').split('\n')[1]
         alignment_string = alignment_string.replace("X", ".")
         score = self._custom_scoring_function(alignment_string, intersection_start,
                                               first_cl_dict['read_limits'], second_cl_dict['read_limits'])
