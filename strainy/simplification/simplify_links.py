f8e4208153bf229f7984c6c3f5a373fc28eecea8
Mikhail Kolmogorov <fenderglass@gmail.com>
2023-03-01 16:30:02 -0800

    only cut links with 1 sibling

--- strainy/simplification/simplify_links.py
+++ strainy/simplification/simplify_links.py
@@ -77,15 +77,19 @@ def clear_links(edge,g):
     changed = False
     to_n = to_neighbours(g, edge, '+')
     if len(to_n) == 1 and g.try_get_segment(to_n[0][0]).dp / edge_cov < cov_ratio:
-        for i in from_neighbours(g,to_n[0][0],to_n[0][1]):
-            if i[0] != edge and len(to_neighbours(g,i[0],i[1])) > 1:
-                changed = remove_link(i[0],i[1], to_n[0][0],to_n[0][1],g)
+        siblings = [u for u in from_neighbours(g,to_n[0][0], to_n[0][1]) if u[0] != edge]
+        if len(siblings) == 1:
+            for i in siblings:
+                if len(to_neighbours(g,i[0],i[1])) > 1:
+                    changed = remove_link(i[0],i[1], to_n[0][0],to_n[0][1],g)
 
     from_n = from_neighbours(g, edge, '+')
     if len(from_n) == 1 and g.try_get_segment(from_n[0][0]).dp / edge_cov < cov_ratio:
-        for i in to_neighbours(g, from_n[0][0], from_n[0][1]):
-            if i[0] != edge and len(from_neighbours(g, i[0], i[1])) > 1:
-                changed = remove_link(from_n[0][0], from_n[0][1],i[0], i[1],g)
+        siblings = [u for u in to_neighbours(g, from_n[0][0], from_n[0][1]) if u[0] != edge]
+        if len(siblings) == 1:
+            for i in siblings:
+                if len(from_neighbours(g, i[0], i[1])) > 1:
+                    changed = remove_link(from_n[0][0], from_n[0][1],i[0], i[1],g)
 
     return changed
 
