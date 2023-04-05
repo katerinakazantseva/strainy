d141bc961a694964828e5fc4a9afc3cb2036ec06
Ekaterina Kazantseva <ekaterina.v.kazantseva@gmail.com>
2023-04-05 14:11:48 +0400

    cluster

--- strainy/clustering/cluster.py
+++ strainy/clustering/cluster.py
@@ -59,79 +59,76 @@ def clusters_vis_stats(G, cl, clN, uncl, SNP_pos, bam, edge, I, AF):
 
 def cluster(i, flye_consensus):
     edge = StRainyArgs().edges[i]
-    try:
-        test=pd.read_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
-    except FileNotFoundError:
-        logger.info("### Reading SNPs...")
-        SNP_pos = read_snp(StRainyArgs().snp, edge, StRainyArgs().bam, AF)
+    logger.info("### Reading SNPs...")
+    SNP_pos = read_snp(StRainyArgs().snp, edge, StRainyArgs().bam, AF)
 
 
-        logger.info("### Reading Reads...")
-        data = read_bam(StRainyArgs().bam, edge, SNP_pos, min_mapping_quality, min_al_len, de_max[StRainyArgs().mode])
+    logger.info("### Reading Reads...")
+    data = read_bam(StRainyArgs().bam, edge, SNP_pos, min_mapping_quality, min_al_len, de_max[StRainyArgs().mode])
+    cl = pd.DataFrame(data={'ReadName': data.keys()})
+    cl['Cluster'] = 'NA'
+
+    total_coverage = 0
+    edge_length = len(read_fasta_seq(StRainyArgs().fa, edge))
+    num_reads = len(data)
+    for read in data:
+        total_coverage += data[read]["Stop"] - data[read]["Start"]
+    mean_edge_cov = total_coverage // edge_length
+    logger.debug(f"num reads: {num_reads}. edge length: {edge_length}, coverage: {mean_edge_cov}")
+
+    if num_reads == 0:
+        return
+    if len(SNP_pos) == 0:
+        #data = read_bam(StRainyArgs().bam, edge, SNP_pos, min_mapping_quality, min_al_len, de_max[StRainyArgs().mode])
         cl = pd.DataFrame(data={'ReadName': data.keys()})
-        cl['Cluster'] = 'NA'
+        cl['Cluster'] = 1
+        cl.to_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
+        return
 
-        total_coverage = 0
-        edge_length = len(read_fasta_seq(StRainyArgs().fa, edge))
-        num_reads = len(data)
-        for read in data:
-            total_coverage += data[read]["Stop"] - data[read]["Start"]
-        mean_edge_cov = total_coverage // edge_length
-        logger.debug(f"num reads: {num_reads}. edge length: {edge_length}, coverage: {mean_edge_cov}")
+    #CALCULATE DISTANCE and ADJ MATRIX
+    logger.info("### Calculatind distances/Building adj matrix...")
+    #try:
+    #    m = pd.read_csv("%s/adj_M/adj_M_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF), index_col='ReadName')
+    #except FileNotFoundError:
+    m = build_adj_matrix(cl, data, SNP_pos, I, StRainyArgs().bam, edge, R)
+    m.to_csv("%s/adj_M/adj_M_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
 
-        if num_reads == 0:
-            return
-        if len(SNP_pos) == 0:
-            #data = read_bam(StRainyArgs().bam, edge, SNP_pos, min_mapping_quality, min_al_len, de_max[StRainyArgs().mode])
-            cl = pd.DataFrame(data={'ReadName': data.keys()})
-            cl['Cluster'] = 1
-            cl.to_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
-            return
+    logger.info("### Removing overweighed egdes...")
+    m = remove_edges(m, R)
 
-        #CALCULATE DISTANCE and ADJ MATRIX
-        logger.info("### Calculatind distances/Building adj matrix...")
-        #try:
-        #    m = pd.read_csv("%s/adj_M/adj_M_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF), index_col='ReadName')
-        #except FileNotFoundError:
-        m = build_adj_matrix(cl, data, SNP_pos, I, StRainyArgs().bam, edge, R)
-        m.to_csv("%s/adj_M/adj_M_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
+    # BUILD graph and find clusters
+    logger.info("### Creating graph...")
+    m1 = m
+    m1.columns = range(0,len(cl['ReadName']))
+    m1.index=range(0,len(cl['ReadName']))
+    G = nx.from_pandas_adjacency(change_w(m.transpose(), R))
+    logger.info("### Searching clusters...")
+    cluster_membership = find_communities(G)
+    clN = 0
+    uncl = 0
 
-        logger.info("### Removing overweighed egdes...")
-        m = remove_edges(m, R)
-
-        # BUILD graph and find clusters
-        logger.info("### Creating graph...")
-        m1 = m
-        m1.columns = range(0,len(cl['ReadName']))
-        m1.index=range(0,len(cl['ReadName']))
-        G = nx.from_pandas_adjacency(change_w(m.transpose(), R))
-        logger.info("### Searching clusters...")
-        cluster_membership = find_communities(G)
-        clN = 0
-        uncl = 0
-
-        for value in set(cluster_membership.values()):
-            group = [k for k, v in cluster_membership.items() if v == value]
-            if len(group) > 3:
-                clN = clN + 1
-                cl['Cluster'][group] = value
-            else:
-                uncl = uncl + 1
-
-        logger.info(str(clN)+" clusters found")
-        cl.to_csv("%s/clusters/clusters_before_splitting_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
-
-        cl.loc[cl['Cluster'] == 'NA', 'Cluster'] = unclustered_group_N
-        if clN != 0:
-            logger.info("### Cluster post-processing...")
-            cl = postprocess(StRainyArgs().bam, cl, SNP_pos, data, edge, R, I, flye_consensus)
+    for value in set(cluster_membership.values()):
+        group = [k for k, v in cluster_membership.items() if v == value]
+        if len(group) > 3:
+            clN = clN + 1
+            cl['Cluster'][group] = value
         else:
-            counts = cl['Cluster'].value_counts(dropna=False)
-            cl = cl[~cl['Cluster'].isin(counts[counts < 6].index)]
-        #clN = len(set(cl.loc[cl['Cluster']!='NA']['Cluster'].values))
-        logger.info(str(clN) + " clusters after post-processing")
-        cl.to_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
-        logger.info("### Graph viz...")
+            uncl = uncl + 1
 
-        clusters_vis_stats (G,cl, clN,uncl,SNP_pos, StRainyArgs().bam, edge, I, AF)
-        cl.to_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
+    logger.info(str(clN)+" clusters found")
+    cl.to_csv("%s/clusters/clusters_before_splitting_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
+
+    cl.loc[cl['Cluster'] == 'NA', 'Cluster'] = unclustered_group_N
+    if clN != 0:
+        logger.info("### Cluster post-processing...")
+        cl = postprocess(StRainyArgs().bam, cl, SNP_pos, data, edge, R, I, flye_consensus)
+    else:
+        counts = cl['Cluster'].value_counts(dropna=False)
+        cl = cl[~cl['Cluster'].isin(counts[counts < 6].index)]
+    #clN = len(set(cl.loc[cl['Cluster']!='NA']['Cluster'].values))
+    logger.info(str(clN) + " clusters after post-processing")
+    cl.to_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
+    logger.info("### Graph viz...")
+
+    clusters_vis_stats (G,cl, clN,uncl,SNP_pos, StRainyArgs().bam, edge, I, AF)
+    cl.to_csv("%s/clusters/clusters_%s_%s_%s.csv" % (StRainyArgs().output, edge, I, AF))
