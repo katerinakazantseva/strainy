37a66587abaf25b74dd4138b8984b041e2aa4e73
atabeerk <ataberk@umd.edu>
2024-03-19 21:25:44 -0400

    fix typo

--- strainy.py
+++ strainy.py
@@ -67,9 +67,7 @@ def main():
                         type=int,
                         default=50)
     parser.add_argument("--only_split",help="Do not run stRainy, only split long gfa unitigs", default='False', required=False)
-
-    parser.add_argument("-Rcl","--Rcl",help="cluster difergence", type=float, required=True)  
-
+    parser.add_argument("-Rcl","--Rcl",help="cluster divergence", type=float, required=True)  
     parser.add_argument("--min-unitig-length",
                         help="The length (in kb) which the unitigs that are shorter will not be phased",
                         required=False,
